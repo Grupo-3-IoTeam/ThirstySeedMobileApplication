a38840913a43f3ac77100bb86d30aac4a5fa31ac
GiaKode <u202118374@upc.edu.pe>
2024-10-29 01:02:26 -0500

    vierPlotStatus

--- lib/ui/screens/menu_screen.dart
+++ lib/ui/screens/menu_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'home_screen.dart';
 import 'account_screen.dart';
 import 'notifications_screen.dart';
+import 'plot_status_screen.dart';
+
 
 class MenuScreen extends StatefulWidget {
   const MenuScreen({super.key});
@@ -19,41 +21,127 @@ class MenuScreenState extends State<MenuScreen> {
     AccountScreen(),
   ];
 
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Thirsty Seed',
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-        centerTitle: true,
-      ),
-      body: _screens[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Inicio',
+      body: Column(
+        children: [
+          // Encabezado personalizado que abarca toda la pantalla
+          Container(
+            width: double.infinity, // Ancho completo
+            color: Colors.green[100],
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: [
+                Icon(
+                  Icons.eco, // Icono de Flutter
+                  color: Colors.green,
+                  size: 40,
+                ),
+                const SizedBox(width: 10),
+                const Text(
+                  'Thirsty Seed',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15,
+                    color: Colors.black,
+                  ),
+                ),
+                
+                
+              ],
+            ),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.notifications),
-            label: 'Notificaciones',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Cuenta',
+          Expanded(
+            child: ListView(
+              padding: const EdgeInsets.symmetric(vertical: 16.0), // Sin padding lateral
+              children: [
+                _buildMenuOption(
+                  icon: Icons.settings,
+                  text: "Administrar parcelas",
+                  color: Colors.green,
+                  onTap: () {
+                    
+                  },
+                ),
+                _buildMenuOption(
+                  icon: Icons.remove_red_eye,
+                  text: "Ver estado de parcelas",
+                  color: Colors.blue,
+                  onTap: () {
+                    // Agregar funcionalidad para "Ver estado de parcelas"
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const PlotStatusScreen()),
+                    );
+                  },
+                ),
+                _buildMenuOption(
+                  icon: Icons.schedule,
+                  text: "Riegos programados",
+                  color: Colors.teal,
+                  onTap: () {
+                    // Agregar funcionalidad para "Riegos programados"
+                  },
+                ),
+                _buildMenuOption(
+                  icon: Icons.insert_chart,
+                  text: "Reportes de riego",
+                  color: Colors.orange,
+                  onTap: () {
+                    // Agregar funcionalidad para "Reportes de riego"
+                  },
+                ),
+                _buildMenuOption(
+                  icon: Icons.notifications,
+                  text: "Notificaciones",
+                  color: Colors.amber,
+                  onTap: () {
+                    setState(() {
+                      _selectedIndex = 1;
+                    });
+                  },
+                ),
+                _buildMenuOption(
+                  icon: Icons.person,
+                  text: "Cuenta",
+                  color: Colors.lightBlue,
+                  onTap: () {
+                    // Navegar a AccountScreen
+                    
+                  },
+                ),
+                const SizedBox(height: 70.0),
+                _buildMenuOption(
+                  icon: Icons.exit_to_app,
+                  text: "Salir",
+                  color: Colors.blueAccent,
+                  onTap: () {
+                    // Agregar funcionalidad para salir de la app
+                  },
+                ),
+              ],
+            ),
           ),
         ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.teal,
-        onTap: _onItemTapped,
+      ),
+    );
+  }
+
+  Widget _buildMenuOption({
+    required IconData icon,
+    required String text,
+    required Color color,
+    required VoidCallback onTap,
+  }) {
+    return Card(
+      elevation: 3,
+      margin: const EdgeInsets.symmetric(vertical: 8.0),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+      child: ListTile(
+        leading: Icon(icon, color: color),
+        title: Text(text),
+        onTap: onTap,
       ),
     );
   }
