7d0c59d8b67c7d37689b9f3ec73f703639864761
Giakomo Causso Mariano <93953227+GiaKode@users.noreply.github.com>
2024-11-18 01:41:25 -0500

    Revert "Feature/nodev1"

--- lib/irrigation/domain/entities/node_entity.dart
+++ lib/irrigation/domain/entities/node_entity.dart
@@ -1,42 +1,22 @@
 class Node {
-  final int id;
-  final int plotId;
   final String location;
   final int moisture;
   final String indicator;
-  final bool isActive;  // Cambié 'status' a 'isActive'
-  final String productcode;
+  final String status;
 
   Node({
-    required this.id,
-    required this.plotId,
     required this.location,
     required this.moisture,
     required this.indicator,
-    required this.isActive,  // Cambié 'status' a 'isActive'
-    required this.productcode,
+    required this.status,
   });
 
   factory Node.fromJson(Map<String, dynamic> json) {
     return Node(
-      id: json['id'] ?? 0,
-      plotId: json['plotId'] ?? 0,
-      location: json['nodelocation'] ?? '',  // Cambié 'location' a 'nodelocation'
-      moisture: json['moisture'] ?? 0,
-      indicator: json['indicator'] ?? '',
-      isActive: json['isActive'] ?? false,  // Cambié 'status' a 'isActive'
-      productcode: json['productcode'] ?? '',
+      location: json['location'],
+      moisture: json['moisture'],
+      indicator: json['indicator'],
+      status: json['status'],
     );
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      'plotId': plotId,
-      'nodelocation': location,  // Cambié 'location' a 'nodelocation'
-      'moisture': moisture,
-      'indicator': indicator,
-      'isActive': isActive,  // Cambié 'status' a 'isActive'
-      'productcode': productcode,
-    };
   }
 }
